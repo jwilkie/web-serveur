cbb307a218c10cb3c35430d6cf439579fbed8813
Jonathan Wilkie <jwilki@lacitec.on.ca>
2024-01-04 14:45:14 -0500

    Remove basepath from useCleanPathname since it's now already supported by Next.js

--- hooks/useCleanPathname.jsx
+++ hooks/useCleanPathname.jsx
@@ -1,17 +1,11 @@
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
-import { basePath } from '@/next.config';
 
 export default function useCleanPathname() {
     const pathname = usePathname();
     
     const getCleanPathname = () => {
         let cleanPathname = pathname;
-
-        // Remove basepath
-        if(basePath) {
-            cleanPathname = cleanPathname.substring(basePath.length);
-        }
 
         // Remove trailing slash
         if(cleanPathname.at(-1) === '/'){
