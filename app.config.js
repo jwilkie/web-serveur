5273a658fe15ac0b52d49f5522386eb6692a857f
Jonathan Wilkie <jwilki@lacitec.on.ca>
2025-08-25 15:46:16 -0400

    Set template to web-serveur

--- app.config.js
+++ app.config.js
@@ -3,19 +3,21 @@ import vsdark from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus
 import html from 'react-syntax-highlighter/dist/cjs/languages/prism/markup'
 import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
 import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
-import csharp from 'react-syntax-highlighter/dist/cjs/languages/prism/csharp'
+import terminal from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
+import handlebars from 'react-syntax-highlighter/dist/cjs/languages/prism/handlebars'
 
 const appConfig = {
-    domain: 'https://jwilkie.github.io/website-template',
-    title: 'Website Template',
-    description: 'Gabarit de base pour des site web de notes de cours.',
+    domain: 'https://jwilkie.github.io/web-serveur',
+    title: 'Web serveur',
+    description: 'Ce cours vous plonge dans l\'univers dynamique du développement côté serveur en utilisant Node.js, un environnement JavaScript puissant, et Express.js, une plateforme légère et flexible. À travers des projets concrets et des démonstrations pratiques, vous apprendrez à concevoir des API REST, gérer des routes, interagir avec des bases de données et sécuriser vos applications web. Une formation essentielle pour bâtir des services web robustes et évolutifs.',
     sectionName: 'module',
     code: {
         languages: {
             'html': { tag: 'HTML', renderer: html },
             'css': { tag: 'CSS', renderer: css },
             'js': { tag: 'Javascript', renderer: js },
-            'csharp': { tag: 'C#', renderer: csharp }
+            'terminal': { tag: 'Terminal', renderer: terminal },
+            'handlebars': { tag: 'Handlebars', renderer: handlebars },
         },
         themes: {
             light: vs,
